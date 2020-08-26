4da732c06b006ac61937386fb7520f0617c41464
Alex2574 <aleksandr.strela@yahoo.com>
2020-08-26 20:21:59 +0300

    Added getCurrentFxRates by axios

--- server/controllers/currencyController.js
+++ server/controllers/currencyController.js
@@ -1,8 +1,16 @@
-exports.getCurrentFxRates = function (req, res) {
+const axios = require('axios');
+
+exports.getCurrentFxRates = async function (req, res) {
   try {
+    await getCurrentRates();
     res.status(200).json('getPostsData');
   } catch (err) {
     res.status(500).json({ status: 500, message: 'err.message' });
   }
 };
 
+async function getCurrentRates() {
+  const res = await axios.get('http://www.lb.lt/webservices/FxRates/FxRates.asmx/getCurrentFxRates?tp=EU');
+  let data = res.data;
+  console.log(data);
+}
