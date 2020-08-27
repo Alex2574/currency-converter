a8b5dc86a5c158dc790047b1484594e3d99311c8
Alex2574 <aleksandr.strela@yahoo.com>
2020-08-27 12:12:46 +0300

    Added mongoose for work with DB

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
 const app = require('./app');
 const http = require('http');
 const PORT = process.env.PORT || 5000;
+const DB_URI = 'mongodb://localhost:27017/convert-currency';
+const mongoose = require('mongoose');
 
-http.createServer(app).listen(PORT);
-console.log('Listening on port: ' + PORT);
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(DB_URI).then(() => {
+    console.log('Listening on port: ' + PORT);
+    http.createServer(app).listen(PORT);
+});
