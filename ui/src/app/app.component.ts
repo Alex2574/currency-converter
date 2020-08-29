c188d2b4d52a925ee954f0d21b21b34ae0fb4515
Alex2574 <aleksandr.strela@yahoo.com>
2020-08-29 20:36:13 +0300

    Add calculate to form

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+  currencies;
   title = 'currency-converter';
+
+  currencyForm = new FormGroup({
+    firstInput: new FormControl(),
+    secondInput: new FormControl(),
+    firstDropdown: new FormControl(),
+    secondDropdown: new FormControl(),
+  });
+
+  private currencySide: boolean;
+  constructor(private dataService: DataService) {
+  }
+
+  ngOnInit() {
+    this.dataService.getAllData().subscribe(response => {
+      console.log(response);
+      this.currencies = response;
+    });
+    this.currencyForm.get('firstInput').valueChanges.subscribe(firstInputValue => {
+      console.log(firstInputValue, this.currencyForm.get('firstDropdown').value);
+      this.currencySide = true;
+    });
+
+    this.currencyForm.get('secondInput').valueChanges.subscribe(secondInputValue => {
+      this.currencySide = false;
+    });
+
+    this.currencyForm.valueChanges.subscribe(allForm => {
+      const firstDropdownValue = this.currencyForm.get('firstDropdown').value;
+      const secondDropdownValue = this.currencyForm.get('secondDropdown').value;
+      if (this.currencySide) {
+        const firstInputValue = this.currencyForm.get('firstInput').value;
+        const result = this.calculate(firstInputValue, firstDropdownValue, secondDropdownValue);
+        this.currencyForm.get('secondInput').patchValue(result, {
+          onlySelf: true,
+          emitEvent: false
+        });
+      } else {
+        const secondInputValue = this.currencyForm.get('secondInput').value;
+        const result = this.calculate(secondInputValue, secondDropdownValue, firstDropdownValue);
+        this.currencyForm.get('firstInput').patchValue(result, {
+          onlySelf: true,
+          emitEvent: false
+        });
+      }
+    });
+  }
+
+  private calculate(firstInputValue, firstDropdownValue, secondDropdownValue) {
+    let result = 0;
+    if (firstInputValue && firstDropdownValue && secondDropdownValue) {
+      result = (firstInputValue / firstDropdownValue) * secondDropdownValue
+    }
+    return result;
+  }
 }
--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
